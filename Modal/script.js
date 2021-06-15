a6a228a0fc7be783938019970b7b185a065bf927
manishkmalla <manishkmalla@gmail.com>
2021-06-15 20:03:43 +0800

    modal project added

--- Modal/script.js
+++ Modal/script.js
@@ -3,5 +3,26 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelector('')
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openmodal = function (){
+    //console.log('Button Clicked');
+    modal.classList.remove('hidden'); // do  not put . in this case
+    overlay.classList.remove('hidden');
+   }
+
+const closemodal = function (){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+   }
+
+for( let i=0; i < btnsOpenModal.length; i++)
+   btnsOpenModal[i].addEventListener('click',openmodal);
+
+
+  
+   btnCloseModal.addEventListener('click', closemodal);
+   overlay.addEventListener('click', closemodal);
+  
+     
+   
