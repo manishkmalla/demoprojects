d1526ef7910329fabca5b3e8c54cdf8ee76eabec
manishkmalla <manishkmalla@gmail.com>
2021-06-21 19:43:49 +0800

    name change

--- Piggame/script.js
+++ Piggame/script.js
@@ -20,15 +20,39 @@ const btnHold = document.querySelector('.btn--hold');
 const winbanner = document.querySelector('.winner-banner');
 
 
-//Starting condition
+let scores, currentScore, activePlayer, playing;
+
+//starting condition
+const init = function(){
+
+  
+ 
 score0El.textContent = 0; //give initial value to 0
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0,0];
-let currentScore = 0;//cannot be inside the function below
-let activePlayer = 0;
-let playing = true;
+scores = [0,0];
+currentScore = 0;//cannot be inside the function below
+activePlayer = 0;
+playing = true;
+  
+  
+  
+  score0El.textContent = 0; //give initial value to 0
+  score1El.textContent = 0;
+  
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');  
+  
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+
+};
+
+//calling initial condition
+init();
+
 
 
 //switch to next player
@@ -39,6 +63,10 @@ const switchPlayer = function () {
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
 };
+
+
+
+
 
 //Rolling dice functionality
 btnRoll.addEventListener('click',function(){
@@ -81,7 +109,7 @@ btnHold.addEventListener('click',function(){
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
  
     //2. Check if player's score is >=100
-    if (scores[activePlayer] >= 50) {
+    if (scores[activePlayer] >= 6) {
         // Finish the game
         playing = false;
         diceEl.classList.add('hidden');
@@ -94,14 +122,13 @@ btnHold.addEventListener('click',function(){
 
         if(activePlayer == 0){
             console.log([activePlayer]);
-            console.log('winner is Sexy Mithra');
-            
+                        
             prompt('Mithra is winner');
             winbanner.textContent ='Winner is Mithra';
             winbanner.classlist.remove('hidden');
         }else{
-            winbanner.textContent ='Winner is Mithra';
-            winbanner.classlist.remove('hidden');
+            //winbanner.textContent ='Winner is Malla';
+            //winbanner.classlist.remove('hidden');
             prompt('Malla is winner');
         }
 
@@ -118,3 +145,9 @@ btnHold.addEventListener('click',function(){
 });
 
 //Reset button
+
+btnNew.addEventListener('click',function(){
+
+  init();
+  
+});
